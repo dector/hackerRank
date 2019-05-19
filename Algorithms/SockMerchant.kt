6933cdde57c53deea6f18bd13b164aede444cf1b
Denys M <dector9@gmail.com>
2019-05-19 16:37:14 +0300

    [Solution] Sock Merchant

---
+++ Algorithms/SockMerchant.kt
@@ -0,0 +1,41 @@
+/* https://www.hackerrank.com/challenges/sock-merchant/ */
+
+import java.io.*
+import java.math.*
+import java.security.*
+import java.text.*
+import java.util.*
+import java.util.concurrent.*
+import java.util.function.*
+import java.util.regex.*
+import java.util.stream.*
+import kotlin.collections.*
+import kotlin.comparisons.*
+import kotlin.io.*
+import kotlin.jvm.*
+import kotlin.jvm.functions.*
+import kotlin.jvm.internal.*
+import kotlin.ranges.*
+import kotlin.sequences.*
+import kotlin.text.*
+
+fun sockMerchant(n: Int, ar: Array<Int>): Int {
+    val uniqueColors = ar.distinct()
+
+    return uniqueColors.map { color ->
+        ar.count { it == color } / 2
+    }.sum()
+}
+
+fun main(args: Array<String>) {
+    val scan = Scanner(System.`in`)
+
+    val n = scan.nextLine().trim().toInt()
+
+    val ar = scan.nextLine().split(" ").map{ it.trim().toInt() }.toTypedArray()
+
+    val result = sockMerchant(n, ar)
+
+    println(result)
+}
+
