1fba409a0fdacdcc5c9d2acf80f6500b47ab6fcc
Denys M <dector9@gmail.com>
2019-05-20 13:10:39 +0300

    [Solution] Minimum Absolute Difference in an Array

---
+++ Algorithms/ds.kt
@@ -0,0 +1,48 @@
+/* https://www.hackerrank.com/challenges/minimum-absolute-difference-in-an-array/ */
+
+import java.io.*
+import java.math.*
+import java.security.*
+import java.text.*
+import java.util.*
+import java.util.concurrent.*
+import java.util.function.*
+import java.util.regex.*
+import java.util.stream.*
+import kotlin.collections.*
+import kotlin.comparisons.*
+import kotlin.io.*
+import kotlin.math.*
+import kotlin.jvm.*
+import kotlin.jvm.functions.*
+import kotlin.jvm.internal.*
+import kotlin.ranges.*
+import kotlin.sequences.*
+import kotlin.text.*
+
+fun minimumAbsoluteDifference(arr: Array<Int>): Int {
+    var diff = Int.MAX_VALUE
+
+    for (i in 0 until arr.lastIndex) {
+        for (j in (i + 1)..arr.lastIndex) {
+            val newDiff = abs(arr[i] - arr[j])
+
+            if (newDiff < diff) diff = newDiff
+        }
+    }
+
+    return diff
+}
+
+fun main(args: Array<String>) {
+    val scan = Scanner(System.`in`)
+
+    val n = scan.nextLine().trim().toInt()
+
+    val arr = scan.nextLine().split(" ").map{ it.trim().toInt() }.toTypedArray()
+
+    val result = minimumAbsoluteDifference(arr)
+
+    println(result)
+}
+
