4a2c7a4b63f6a46dee6772b166396fd80bf638ca
Denys M <dector9@gmail.com>
2019-11-14 11:54:50 +0400

    `FunnyString` template.

---
+++ Algorithms/FunnyString.kt
@@ -0,0 +1,17 @@
+/* https://www.hackerrank.com/challenges/funny-string/ */
+
+fun foo() {
+
+}
+
+fun main() {
+    val scan = Scanner(System.`in`)
+    val q = scan.nextLine().trim().toInt()
+
+    for (i in 1..q) {
+        val s = scan.nextLine().trim().toInt()
+
+        val result = foo(n)
+        println(result)
+    }
+}
